5b5273d392502d56b61a85baef6862100d8be057
Shruti Sawant <shrutisawant@Shrutis-MacBook-Pro.local>
2025-03-18 14:11:25 -0700

    add create table queries for hospitals and hospital ratings and merge create table queries into one file

--- Create_table_queries.sql
+++ Create_table_queries.sql
@@ -16,3 +16,50 @@ CREATE TABLE taxonomy_data (
     CONSTRAINT Taxonomy_code_check
         CHECK (PROVIDER_TAXONOMY_CODE ~ '^[A-Za-z0-9]+$')
 );
+
+CREATE TABLE Hospitals (
+    OSHPD_ID INT PRIMARY KEY,
+    FacilityName VARCHAR(255) NOT NULL,
+    FacilityType VARCHAR(255),
+    Address VARCHAR(255),
+    Address2 VARCHAR(255),
+    City VARCHAR(255),
+    State VARCHAR(2),
+    ZIP INT,
+    County VARCHAR(255),
+    Facility_level_desc VARCHAR(255),
+    Total_number_beds INT,
+    Er_service_level_desc VARCHAR(255),
+    Facility_status_desc VARCHAR(255) CHECK (License_type_desc IN ('Open', 'Suspense')),
+    License_type_desc VARCHAR(255) CHECK (License_type_desc IN ('Clinic', 'Home health', 'Hospital', 'Long term care facility')),
+    License_category_desc VARCHAR(255)
+);
+
+CREATE TABLE hospital_ratings (
+    id SERIAL PRIMARY KEY,
+    OSHPD_ID INT REFERENCES hospitals(OSHPD_ID),
+    Performance_measure VARCHAR(255) NOT NULL,
+    No_of_adverse_events INT,
+    No_of_cases INT,
+    Risk_adjusted_rate FLOAT,
+    Hospital_ratings VARCHAR(255)
+);
+
+CREATE TABLE ProviderNetwork (
+    ProviderID SERIAL PRIMARY KEY,
+    FirstName TEXT NOT NULL,
+    LastName TEXT NOT NULL,
+    ManagedCarePlan TEXT NOT NULL,
+    SubNetwork TEXT,
+    NPI INTEGER NOT NULL,
+    Taxonomy VARCHAR CHECK (Taxonomy ~ '^[a-zA-Z0-9]+$'),
+    MCNAProviderGroup TEXT,
+    MCNAProviderType TEXT,
+    LicensureType VARCHAR(4),
+    PrimaryCare CHAR(1) CHECK (PrimaryCare IN ('Y', 'N')),
+    Specialist CHAR(1) CHECK (Specialist IN ('Y', 'N')),
+    SeesChildren TEXT CHECK (SeesChildren IN ('Both', 'Only', 'No')),
+    Telehealth TEXT CHECK (Telehealth IN ('Both', 'Only', 'No')),
+    BHIndicator TEXT,
+    OSHPD_ID INTEGER,
+);
